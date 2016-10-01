c6fadf2dbf84b0188bdce689afdea4c31a5d577b
kolya-t <toporkov.kolya96@gmail.com>
2016-10-01 17:40:51 +0300

    project creat

---
+++ android/src/com/bordergames/tekikaranigete/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.bordergames.tekikaranigete;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bordergames.tekikaranigete.TekiKaraNigete;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TekiKaraNigete(), config);
+	}
+}
